a0f6c1db1ebd0e82aeb4d71b72afaed2575f27ae
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-09 15:01:10 -0500

    Refactor: Validates against static list of acceptable currency and inverted if statement to simplify

--- VendingMachine/VendingMachine/VendingMachine/Validators/MoneyValidator.cs
+++ VendingMachine/VendingMachine/VendingMachine/Validators/MoneyValidator.cs
@@ -1,12 +1,14 @@
-﻿namespace VendingMachine.VendingMachine.Validators
+﻿using System.Collections.Generic;
+
+namespace VendingMachine.VendingMachine.Validators
 {
     public class MoneyValidator
     {
+        private List<string> _validCoinList = new List<string> { "Nickel", "Dime", "Quarter" };
+
         public bool IsValidCoin(string coin)
         {
-            if (coin == "Nickel" || coin == "Dime"  || coin == "Quarter")
-                return true;
-            return false;
+            return _validCoinList.Contains(coin);
         }
     }
 }
