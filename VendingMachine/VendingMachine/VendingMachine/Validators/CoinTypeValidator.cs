8eeb9fd2aaee807042a0ee72824eccf4f54d97ec
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-09 22:13:03 -0500

    Red: added test to validate weight and diameter to determine coin type

---
+++ VendingMachine/VendingMachine/VendingMachine/Validators/CoinTypeValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace VendingMachine.VendingMachine.Validators
+{
+    public class CoinTypeValidator
+    {
+
+        public CoinTypes Identify(double weight, double diameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum CoinTypes
+    {
+        Penny = 0,
+        Nickel = 1,
+        Dime = 2,
+        Quarter =3
+    }
+}
---
+++ VendingMachine/VendingMachine/VendingMachineTests/ValidatorsTests/CoinTypeValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using VendingMachine.VendingMachine.Validators;
+
+namespace VendingMachine.VendingMachineTests.ValidatorsTests
+{
+    [TestFixture]
+    public class CoinTypeValidatorTests
+    {
+        private CoinTypeValidator _coinValidator = new CoinTypeValidator();
+        private double _diameter;
+        private double _weight;
+
+        [Test]
+        public void IdentifyShouldDetermineDime()
+        {
+            _diameter = 2.268;
+            _weight = 17.91;
+
+            var actual = _coinValidator.Identify(_weight, _diameter);
+
+            Assert.AreEqual(CoinTypes.Dime, actual);
+        }
+    }
+}
