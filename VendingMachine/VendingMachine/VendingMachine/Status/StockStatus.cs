3ed0c07f5dda3be8ce1a3a51fc536bdca867a8aa
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-11 08:06:27 -0500

    Red: Added class to hold inventory state and update values

---
+++ VendingMachine/VendingMachine/VendingMachine/Status/StockStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using VendingMachine.Models;
+
+namespace VendingMachine.VendingMachine.Status
+{
+    public class StockStatus
+    {
+        public static int AddInventory(VendingStock stock, int addedInventory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VendingMachine/VendingMachine/VendingMachineTests/StatusTests/StockStatusTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using VendingMachine.Models;
+using VendingMachine.VendingMachine.Status;
+
+namespace VendingMachine.VendingMachineTests.StatusTests
+{
+    [TestFixture]
+    public class StockStatusTests
+    {
+        private StockStatus _stockStatus = new StockStatus();
+
+        [Test]
+        public void AddInventoryIncreasesTotalStockByTwo()
+        {
+            var actual = StockStatus.AddInventory(VendingStock.Candy, 2);
+
+            Assert.AreEqual(2, actual);
+        }
+    }
+
+}
