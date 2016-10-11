5dddee1acbeada05d6a4440a28048be1f99d122f
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-11 13:48:42 -0500

    Green: added coverage to add to chip totals

--- VendingMachine/VendingMachine/VendingMachine/Status/StockStatus.cs
+++ VendingMachine/VendingMachine/VendingMachine/Status/StockStatus.cs
@@ -6,8 +6,9 @@ namespace VendingMachine.VendingMachine.Status
 {
     public class StockStatus
     {
-        private static int _candyStock = 0;
-        private static int _popStock = 0;
+        private static int _candyStock;
+        private static int _popStock;
+        private static int _chipsStock;
 
         public static int AddInventory(VendingStock stock, int addedInventory)
         {
@@ -20,6 +21,11 @@ namespace VendingMachine.VendingMachine.Status
             {
                 _popStock += addedInventory;
                 return _popStock;
+            }
+            if (stock == VendingStock.Chips)
+            {
+                _chipsStock += addedInventory;
+                return _chipsStock;
             }
             return 0;
         }
