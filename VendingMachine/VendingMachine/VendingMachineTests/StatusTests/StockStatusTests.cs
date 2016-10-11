612c45bfc46dc49e9ff4a35d5e5afed5f5b6d9b5
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-11 12:04:06 -0500

    Red: Added test to validate AddInventory will increase stock of Pop

--- VendingMachine/VendingMachine/VendingMachineTests/StatusTests/StockStatusTests.cs
+++ VendingMachine/VendingMachine/VendingMachineTests/StatusTests/StockStatusTests.cs
@@ -7,14 +7,20 @@ namespace VendingMachine.VendingMachineTests.StatusTests
     [TestFixture]
     public class StockStatusTests
     {
-        private StockStatus _stockStatus = new StockStatus();
-
         [Test]
-        public void AddInventoryIncreasesTotalStockByTwo()
+        public void AddInventoryIncreasesTotalCandyStock()
         {
             var actual = StockStatus.AddInventory(VendingStock.Candy, 2);
 
             Assert.AreEqual(2, actual);
+        }
+
+        [Test]
+        public void AddInventoryIncreasesTotalPopStock()
+        {
+            var actual = StockStatus.AddInventory(VendingStock.Pop, 5);
+
+            Assert.AreEqual(5, actual);
         }
     }
 
