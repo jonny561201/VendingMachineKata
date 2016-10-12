6629ebbdafb8f5086fedc575e85c6adae8d04726
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-11 21:42:32 -0500

    Refactor: Added coin type validation onto the enum coin type to simplify code

--- VendingMachine/VendingMachine/Models/Coins.cs
+++ VendingMachine/VendingMachine/Models/Coins.cs
@@ -14,20 +14,22 @@ namespace VendingMachine.Models
 
         static Coin ()
         {
-            Penny = new Coin(2.5,19.05);
-            Nickel = new Coin(5,21.21);
-            Dime = new Coin(2.268,17.91);
-            Quarter = new Coin(5.67,24.26);
-            HalfDollar = new Coin(11.34,30.61);
-            Dollar = new Coin(8.1,26.5);
-            Unknown = new Coin(0,0);
+            Penny = new Coin(2.5,19.05, false);
+            Nickel = new Coin(5,21.21, true);
+            Dime = new Coin(2.268,17.91, true);
+            Quarter = new Coin(5.67,24.26, true);
+            HalfDollar = new Coin(11.34,30.61, false);
+            Dollar = new Coin(8.1,26.5, false);
+            Unknown = new Coin(0,0, false);
         }
 
         public double Weight { get; }
         public double Height { get; }
+        public bool IsValidTender { get; }
 
-        private Coin(double weight, double height)
+        private Coin(double weight, double height, bool isvalidTender)
         {
+            IsValidTender = isvalidTender;
             Weight = weight;
             Height = height;
         }
