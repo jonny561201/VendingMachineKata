53e0993203b1918d531725792b362e5206af3f71
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-12 21:35:58 -0500

    Red: Added test to verify selection will ultimately purchase an item from vending machine

--- VendingMachine/VendingMachine/VendingMachineTests/ControllersTests/SelectItemControllerTests.cs
+++ VendingMachine/VendingMachine/VendingMachineTests/ControllersTests/SelectItemControllerTests.cs
@@ -13,20 +13,25 @@ namespace VendingMachine.VendingMachineTests.ControllersTests
         private SelectItemController _controller;
         private Mock<IStockStatus> _stockStatus; 
         private Mock<IStockPurchaseValidator> _itemValidator;
+        private readonly VendingStock _stock = VendingStock.Candy;
+        private const decimal TenderedAmount = 0.65m;
 
-        [Test]
-        public void SelectEvaluatesAvailabilityOfSelection()
+        [SetUp]
+        public void SetUp()
         {
             _itemValidator = new Mock<IStockPurchaseValidator>();
             _stockStatus = new Mock<IStockStatus>();
             _controller = new SelectItemController(_itemValidator.Object, _stockStatus.Object);
+        }
 
-            var stock = VendingStock.Candy;
-            var tenderedAmount = 0.65m;
-            _controller.Select(stock, tenderedAmount);
+        [Test]
+        public void SelectEvaluatesAvailabilityOfSelection()
+        {
+            _controller.Select(_stock, TenderedAmount);
 
-            _stockStatus.Verify(x => x.HasAvailableItem(stock), Times.Once);
-            _itemValidator.Verify(x => x.CanPurchase(stock, tenderedAmount), Times.Once);
+            _stockStatus.Verify(x => x.HasAvailableItem(_stock), Times.Once);
+            _itemValidator.Verify(x => x.CanPurchase(_stock, TenderedAmount), Times.Once);
+            _stockStatus.Verify(x => x.PurchaseItem(_stock), Times.Once);
         }
     }
 }
