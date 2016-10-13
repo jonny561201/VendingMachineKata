2643a5901c6c6fc46d3b82250bc9026d800f6556
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-12 21:18:47 -0500

    Green: HasAvailableItem is now being called from controller

--- VendingMachine/VendingMachine/VendingMachine/Controllers/SelectItemController.cs
+++ VendingMachine/VendingMachine/VendingMachine/Controllers/SelectItemController.cs
@@ -8,14 +8,17 @@ namespace VendingMachine.VendingMachine.Controllers
     public class SelectItemController
     {
         private readonly IStockPurchaseValidator _itemValidator;
+        private readonly IStockStatus _stockStatus;
 
         public SelectItemController(IStockPurchaseValidator itemValidator, IStockStatus stockStatus)
         {
             _itemValidator = itemValidator;
+            _stockStatus = stockStatus;
         }
 
         public void Select(VendingStock vendingStock, decimal tenderedAmount)
         {
+            _stockStatus.HasAvailableItem(vendingStock);
             _itemValidator.CanPurchase(vendingStock, tenderedAmount);
         }
     }
