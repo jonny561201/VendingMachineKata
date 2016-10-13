becef9a27a55c2cd6bb9d1d46545b47d305b865b
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-13 15:39:02 -0500

    Red: added new class to track total change in machine and test to ensure deposit increases total

---
+++ VendingMachine/VendingMachine/VendingMachine/Status/AvailableChangeStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.Models;
+
+namespace VendingMachine.VendingMachine.Status
+{
+    public class AvailableChangeStatus
+    {
+        public decimal DepositChange(VendingStock stock)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VendingMachine/VendingMachine/VendingMachineTests/StatusTests/AvailableChangeStatusTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using VendingMachine.Models;
+using VendingMachine.VendingMachine.Status;
+
+namespace VendingMachine.VendingMachineTests.StatusTests
+{
+    [TestFixture]
+    public class AvailableChangeStatusTests
+    {
+        private AvailableChangeStatus _changeStatus;
+        private VendingStock _stock;
+
+        [Test]
+        public void DepositChangeWillIncreaseTotalAvailableChangeByStockCost()
+        {
+            _changeStatus = new AvailableChangeStatus();
+            _stock = VendingStock.Pop;
+
+            var actual = _changeStatus.DepositChange(_stock);
+
+            Assert.AreEqual(_stock.Cost, actual);
+        }
+    }
+}
