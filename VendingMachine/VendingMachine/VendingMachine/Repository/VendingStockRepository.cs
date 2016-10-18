79ada22ac48017bf037d20942fa1b46f1585c60c
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-18 13:53:09 -0500

    Red: creating seam for potential database integration through test of returning current stock items

---
+++ VendingMachine/VendingMachine/VendingMachine/Repository/VendingStockRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.VendingMachine.Status;
+
+namespace VendingMachine.VendingMachine.Repository
+{
+    public class VendingStockRepository
+    {
+        public IEnumerable<StockItem> GetInventory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VendingMachine/VendingMachine/VendingMachineTests/RepositoryTests/VendingStockRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VendingMachine.Models;
+using VendingMachine.VendingMachine.Repository;
+using VendingMachine.VendingMachine.Status;
+
+namespace VendingMachine.VendingMachineTests.RepositoryTests
+{
+    [TestFixture]
+    public class VendingStockRepositoryTests
+    {
+        [Test]
+        public void GetInventoryWillReturnListOfCurrentStock()
+        {
+            var stockRepo = new VendingStockRepository();
+            var expectedStock = new List<StockItem>{};
+
+            var totalStock = stockRepo.GetInventory();
+
+            CollectionAssert.AreEquivalent(expectedStock, totalStock);
+        }
+
+    }
+}
