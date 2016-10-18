550164cbaf4f90734a476ac5f5e6a1f3ecf99061
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-18 14:01:11 -0500

    Green: Stock repository will return a list of stock items

--- VendingMachine/VendingMachine/VendingMachine/Repository/VendingStockRepository.cs
+++ VendingMachine/VendingMachine/VendingMachine/Repository/VendingStockRepository.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VendingMachine.Models;
 using VendingMachine.VendingMachine.Status;
 
 namespace VendingMachine.VendingMachine.Repository
 {
     public class VendingStockRepository
     {
+        private readonly List<StockItem> _stock = new List<StockItem> {  };
+
         public IEnumerable<StockItem> GetInventory()
         {
-            throw new NotImplementedException();
+            return _stock;
         }
     }
 }
