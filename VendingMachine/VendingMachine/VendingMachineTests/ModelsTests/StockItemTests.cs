f71758242c5b266d6f195947515660ca2ed39f55
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-18 20:37:16 -0500

    Red: Added test to assert StockItems can be compared equivalently by properties

---
+++ VendingMachine/VendingMachine/VendingMachineTests/ModelsTests/StockItemTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using VendingMachine.Models;
+
+namespace VendingMachine.VendingMachineTests.ModelsTests
+{
+
+    [TestFixture]
+    public class StockItemTests
+    {
+        [Test]
+        public void EqualsWillReturnTrueWhenPropertiesMatch()
+        {
+            var stock1 = new StockItem(VendingStock.Candy, 1);
+            var stock2 = new StockItem(VendingStock.Candy, 1);
+
+            Assert.AreEqual(stock1, stock2);
+        }
+
+    }
+}
