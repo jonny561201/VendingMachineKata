e3584e19173e0893f39cfe1efa56be3da5a4baae
Jonathon Graf <grafjonathan@johndeere.com>
2016-10-18 21:24:42 -0500

    Green: Added additional test coverage for VendingStock Repo to increase list of items according to stock type

--- VendingMachine/VendingMachine/VendingMachineTests/RepositoryTests/VendingStockRepositoryTests.cs
+++ VendingMachine/VendingMachine/VendingMachineTests/RepositoryTests/VendingStockRepositoryTests.cs
@@ -27,11 +27,31 @@ namespace VendingMachine.VendingMachineTests.RepositoryTests
         }
 
         [Test]
-        public void AddInventoryWillUpdateListOfCurrentStock()
+        public void AddInventoryWillUpdateListOfCurrentStockForPop()
         {
             var expectedStock = new List<StockItem> { new StockItem(VendingStock.Candy, 0), new StockItem(VendingStock.Chips, 0), new StockItem(VendingStock.Pop, 10) };
 
             var actual = _stockRepo.AddInventory(VendingStock.Pop, 10);
+
+            CollectionAssert.AreEquivalent(expectedStock, actual);
+        }
+
+        [Test]
+        public void AddInventoryWillUpdateListOfCurrentStockForChips()
+        {
+            var expectedStock = new List<StockItem> { new StockItem(VendingStock.Candy, 0), new StockItem(VendingStock.Chips, 5), new StockItem(VendingStock.Pop, 0) };
+
+            var actual = _stockRepo.AddInventory(VendingStock.Chips, 5);
+
+            CollectionAssert.AreEquivalent(expectedStock, actual);
+        }
+
+        [Test]
+        public void AddInventoryWillUpdateListOfCurrentStockForCandy()
+        {
+            var expectedStock = new List<StockItem> { new StockItem(VendingStock.Candy, 7), new StockItem(VendingStock.Chips, 0), new StockItem(VendingStock.Pop, 0) };
+
+            var actual = _stockRepo.AddInventory(VendingStock.Candy, 7);
 
             CollectionAssert.AreEquivalent(expectedStock, actual);
         }
